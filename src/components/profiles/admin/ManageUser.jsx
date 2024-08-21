0a00f1a075565220fca33a2eb530d3d3746c4461
nohaiz <nohaiznazier@gmail.com>
2024-08-21 20:06:06 +0300

    completed css from my part

--- src/components/profiles/admin/ManageUser.jsx
+++ src/components/profiles/admin/ManageUser.jsx
@@ -24,78 +24,69 @@ const ManageUsers = ({ user, handleDeleteUser }) => {
   }, [userType]);
 
   return (
-    <div className="full-height-center">
-      <div className="box">
-        <div className="container mt-5">
-          <div className="columns is-vcentered mb-4">
-            <div className="column is-12">
-              <div className="buttons">
-                <button className="button is-primary" onClick={() => setUserType("admins")}>Admins</button>
-                <button className="button is-primary" onClick={() => setUserType("patients")}>Patients</button>
-                <button className="button is-primary" onClick={() => setUserType("doctors")}>Doctors</button>
-              </div>
+    <div className="section is-small">
+      <div className="box has-background-white">
+        <div className="columns is-vcentered mb-4">
+          <div className="column is-12">
+            <div className="buttons custom-placement">
+              <button className="button custom-button" onClick={() => setUserType("admins")}>Admins</button>
+              <button className="button custom-button" onClick={() => setUserType("patients")}>Patients</button>
+              <button className="button custom-button" onClick={() => setUserType("doctors")}>Doctors</button>
+              {(userType === "admins" || userType === "doctors") && (
+                <Link to={`/users/${userType}`}>
+                  <button className="button custom-button-create">+</button>
+                </Link>
+              )}
             </div>
           </div>
-          <div className="columns is-multiline">
-            {data ? (
-              data.map((users, index) => (
-                <div key={index} className="column is-4-desktop is-6-tablet">
-                  <div className="box">
-                    <div className="title is-5">
-                      {user.type[2000] === users._id ? (
-                        <div hidden="true"></div>
+        </div>
+        <p className="user-type-title">{userType.charAt(0).toUpperCase() + userType.slice(1)}</p>
+        {data.length > 0 ? (
+          data.map((users, index) => (
+            user.type[2000] !== users._id && ( 
+              <div key={index} className="column">
+                <div className="box" style={{ backgroundColor: '#f0f0f0'}}>
+                  <div className="title is-5">
+                    <div className="custom-content" style={{ color: 'hsl(0, 0%, 71%)' }}>
+                      {userType === "doctors" ? (
+                        <Link to={`/users/doctors/${users._id}`} className="link-dark">
+                          {`${users.firstName} ${users.lastName}`}
+                        </Link>
+                      ) : userType === "patients" ? (
+                        <Link to={`/users/patients/${users._id}`} className="link-dark">
+                          {`${users.firstName} ${users.lastName}`}
+                        </Link>
                       ) : (
-                        <div>
-                          {userType === "doctors" ? (
-                            <Link to={`/users/doctors/${users._id}`} className="has-text-link">
-                              {`${users.firstName} ${users.lastName}`}
-                            </Link>
-                          ) : userType === "patients" ? (
-                            <Link to={`/users/patients/${users._id}`} className="has-text-link">
-                              {`${users.firstName} ${users.lastName}`}
-                            </Link>
-                          ) : (
-                            `${users.firstName} ${users.lastName}`
-                          )}
-                          <button
-                            className="button is-danger is-small mt-2"
-                            type="button"
-                            onClick={async () => {
-                              try {
-                                await handleDeleteUser(userType, users._id);
-                                await fetchUser();
-                              } catch (error) {
-                                console.error("Error during delete and fetch: ", error);
-                              }
-                            }}
-                          >
-                            Delete
-                          </button>
-                        </div>
+                        `${users.firstName} ${users.lastName}`
                       )}
+                      <button
+                        className="button is-danger"
+                        type="button"
+                        onClick={async () => {
+                          try {
+                            await handleDeleteUser(userType, users._id);
+                            await fetchUser();
+                          } catch (error) {
+                            console.error("Error during delete and fetch: ", error);
+                          }
+                        }}
+                      >
+                        Delete
+                      </button>
                     </div>
                   </div>
                 </div>
-              ))
-            ) : (
-              <div className="column is-full">
-                <p className="has-text-centered">No data available</p>
               </div>
-            )}
+            )
+          ))
+        ) : (
+          <div className="column is-full">
+            <p className="has-text-centered">No data available</p>
           </div>
-  
-          {(userType === "admins" || userType === "doctors") && (
-            <div className="columns">
-              <div className="column is-full">
-                <Link to={`/users/${userType}`}>
-                  <button className="button is-primary">Create</button>
-                </Link>
-              </div>
-            </div>
-          )}
-        </div>
+        )}
       </div>
     </div>
   );
 };
+
 export default ManageUsers;
